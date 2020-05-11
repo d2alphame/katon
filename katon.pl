671e6e121202a6eae8dfbfb1705163d7e8fda3b3
Deji Adegbite <d2alphame@yahoo.com>
2020-05-11 21:20:11 +0100

    Match balanced brackets within body of text

--- katon.pl
+++ katon.pl
@@ -13,35 +13,55 @@ use v5.26;          # Use Perl of at least this version
 open(my $tmp, "+>", undef) or die "Could not create temporary file";
 
 my $declarations = "";                                  # Save declarations here as we find them
+my $code_blocks = "";                                   # Save code blocks here.
 my $var = qr/[a-zA-Z_][a-zA-Z0-9_]*/;                   # Regex for recognizing variable names
+my $assign = qr/$var\s*=\s*(['"`]).+?\1/;               # Match assigning values
 
 
 # Read all lines from all files passed as arguments from the command line
 while(<>) {
 
     # Match, for example:
-    # $x = 'My val' or 
-    # $x = `My val`
-    if(/^\s*(\$$var)\s*=\s*(['`])([^\2]+?)\2\s*$/) {
-        $declarations .= "my $1 = qq$2$3$2;\n";
+    # x = 'My val' or 
+    # x = `My val`
+    if(/^\s*($var)\s*=\s*(['`])([^\2]+?)\2\s*$/) {
+        $declarations .= 'my $' . "$1 = qq$2$3$2;\n";
         next;
     }
     # Match assigning with double-quotes character
     # Matches e.g. $x = "My Value"
-    elsif(/^\s*(\$$var)\s*=\s*["]([^"]+?)"\s*$/) {
-        $declarations .= qq(my $1 = "$2";\n);
+    elsif(/^\s*($var)\s*=\s*["]([^"]+?)"\s*$/) {
+        $declarations .= 'my $' . qq($1 = "$2";\n);
         next;
     }
 
 
     # Match for code-fence. Code fences are blocks of
-    # actual Perl code
+    # literal Perl code
     if(/^\s*```\s*$/) {
-       while(<>) {
+        while(<>) {
            last if /^\s*```\s*$/;
            $declarations .= $_;
-       }
+        }
+        next;
+    }
 
+
+    # Match declare-and-assign inside a body of text
+    # This is done with $(...), $<...>, $[...], ${...}
+    # Any of the 4 brackets can be used and the assignment
+    # goes between them
+    if(/\$([\({<\[])/) {
+        my $left_bracket = $1;
+        my $right_bracket;
+        if      ($1 eq '(') { $right_bracket = ')' }
+        elsif   ($1 eq '<') { $right_bracket = '>' }
+        elsif   ($1 eq '[') { $right_bracket = ']' }
+        elsif   ($1 eq '{') { $right_bracket = '}' }
+
+        if(/\$$left_bracket([^$right_bracket]+?)$right_bracket/) {
+            
+        }
     }
 
 }
